22c46a555241ad19fa74713591da227b9756c11c
Frank <lokenator73@gmail.com>
2019-04-17 10:29:21 -0400

    Added Global.asax file to map route with no .svc extension

---
+++ AllForOneGymService/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AllForOneGymService.Global" Language="C#" %>
---
+++ AllForOneGymService/Global.asax.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.ServiceModel.Activation;
+using System.Web.Routing;
+
+namespace AllForOneGymService
+{
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            RouteTable.Routes.Add(new ServiceRoute("Client", new WebServiceHostFactory(), typeof(ClientService)));
+
+            RouteTable.Routes.Add(new ServiceRoute("Trainer", new WebServiceHostFactory(), typeof(TrainerService)));
+        }
+
+        protected void Session_Start(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_AuthenticateRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Session_End(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Application_End(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
